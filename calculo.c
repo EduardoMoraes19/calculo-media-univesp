2128124eb257f2c34641b916216ef8885262988c
Eduardo Moraes <131202382+EduardoMoraes19@users.noreply.github.com>
2025-07-02 17:52:32 -0300

    fix/changing-variable-names

--- calculo.c
+++ calculo.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 int main(){
-    float n_prova, n_atividade, m_prova, m_atividades, m_final;
+    float nota_prova, nota_atividade, media_prova, media_atividades, media_final;
     
     printf("Digite as notas de prova e da media das atividades avaliativas para calcular sua media.\n");
 
-    printf("Digite o nota da prova\n");
-    scanf("%f", &n_prova);
+    printf("Digite o nota da prova:\n");
+    scanf("%f", &nota_prova);
 
-    printf("Digite o media das atividades avaliativas\n");
-    scanf("%f", &n_atividade);
+    printf("Digite o media das atividades avaliativas:\n");
+    scanf("%f", &nota_atividade);
 
-    m_prova = n_prova * 0.6;
-    m_atividades = n_atividade * 0.4;
+    media_prova = nota_prova * 0.6;
+    media_atividades = nota_atividade * 0.4;
     
-    m_final = m_prova + m_atividades;
+    media_final = media_prova + media_atividades;
 
-    if (m_final >= 6.0 && m_final <= 10.0)
+    if (media_final >= 6.0 && media_final <= 10.0)
     {
-        printf("Você passou nessa materia sua media é %.2f", m_final);
+        printf("Você passou nessa materia sua media é %.2f", media_final);
     }else{
-        printf("Você esta de exame sua nota foi %.2f", m_final);
+        printf("Você esta de exame sua nota foi %.2f", media_final);
     }
     
 }
